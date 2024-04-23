6fc1ac32742f3b6a2275b44193c17ccf5a47f36f
Abmaellf <abmael.ninha@gmail.com>
2024-04-23 09:33:23 -0400

    feat( 21. Ganchos e campos obrigatorios)

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -36,5 +36,61 @@ describe('POST /users', () =>{
 			})  
   })
 
+  context('required fields',() => {
 
+    let user;
+
+    // Esse gancho sempre será executado quando houver um it(cenário)
+    beforeEach(() =>{
+      user = {
+        name: 'margo',
+        email: 'margo@hotmail.com',
+        password: '141620'
+      }
+    })
+
+    it('name is required',() => {
+
+      delete user.name
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"name\" is required')
+        })
+    })
+
+    it('email is required',() => {
+
+      delete user.email
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"email\" is required')
+        })
+    })
+
+
+    it('password is required',() => {
+
+      delete user.password
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"password\" is required')
+        })
+    })
+
+  })
 })
