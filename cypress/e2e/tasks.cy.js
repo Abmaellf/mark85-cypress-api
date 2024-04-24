c9e1433998a9df5d74f43526e4201de54de5ed8a
Abmaellf <abmael.ninha@gmail.com>
2024-04-24 14:00:54 -0400

    26. Adicionando o token no header

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,34 @@
+describe('POST /task', () => {
+
+   beforeEach(function () {
+    cy.fixture('tasks').then(function (tasks){
+        this.tasks =  tasks
+    })
+   })
+
+    it('register a new task', function() {
+        
+        const { user, task } = this.tasks.create
+
+        cy.postSession(user)
+            .then(response => {
+                cy.log(response.body.token)
+
+                cy.api({
+                    url: '/tasks',
+                    method: 'POST',
+                    body: task,
+                    headers: {
+                        authorization: response.body.token
+                    },
+                    failOnStatusCode: false
+                }).then(response => {
+                    expect(response.status).to.eq(200)
+                })
+                Cypress.env('token', response.body.token)
+            })
+
+        
+    })
+
+})
